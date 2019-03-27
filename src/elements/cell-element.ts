b214ce4c0a6d7f5b75d9a8cbab28bb463ec954f5
Darren Hill <turboignited@gmail.com>
2019-03-27 21:16:42 +0000

    Changed to show a random colour

--- src/elements/cell-element.ts
+++ src/elements/cell-element.ts
@@ -1,24 +1,25 @@
 import Cell from "../cell";
 import { BodyElement } from "./body-element";
-import { createDiv } from "./elements";
+import { createDiv, randomColourString } from "./elements";
 
 interface CellElementConstructor {
     cell: Cell;
-    element?: HTMLElement;
 }
+
 export default class CellElement extends BodyElement {
     private _cell: Cell;
+    private _colour: string = randomColourString();
+
     constructor(args: CellElementConstructor) {
-        super({ element: args.element ? args.element : createDiv() });
+        super({ element: createDiv() });
         this._cell = args.cell;
         this.updateStyle();
     }
 
     public updateStyle(): void {
-        //        this.setStyle("background", this._colour);
+        this.setStyle("background", this._colour);
         this.setStyle("grid-row", `${this._cell.position.y + 1} / span ${this._cell.span.y}`);
         this.setStyle("grid-column", `${this._cell.position.x + 1} / span ${this._cell.span.x}`);
-        this.setStyle("border", "1px solid black");
     }
 
 }
